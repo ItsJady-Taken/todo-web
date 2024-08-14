266ff691abc7fddf795959bf20b148d29da75815
ItsJady-Taken <davidkhun2004@gmail.com>
2024-08-14 21:19:42 +0700

    decluttered the html and move the modal to JS file

--- src/app/createTodo.js
+++ src/app/createTodo.js
@@ -1,11 +1,30 @@
+let close;
+let modal;
+function todoModal() {
+    if (!modal) {
+        modal  = document.createElement('div');
+        modal.classList.add('modal');
+        modal.id = 'myModal';
+        modal.innerHTML = `
+        
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <h2>Create Todo</h2>
+                <p>This is a simple modal popup example.</p>
+                <button id="modalButton">Okay</button>
+            </div>
+                
+        `;
+        document.getElementById('main-content').appendChild(modal);
+        close = document.querySelector('.close');
+    }
+    return modal;
+}
 export default function createTodo() { 
-
-    const close = document.querySelector('.close');
-    const modal = document.getElementById('myModal');
-    modal.style.display = "flex";
-    setTimeout(() => modal.classList.add('show'), 10);
+    todoModal().style.display = "flex";
+    setTimeout(() => todoModal().classList.add('show'), 10);
     close.onclick = ()=> {
-        setTimeout(() => modal.style.display = "none", 300);
-        modal.classList.remove('show');
+        setTimeout(() => todoModal().style.display = "none", 300);
+        todoModal().classList.remove('show');
     }
 }
