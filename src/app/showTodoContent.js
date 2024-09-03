4cd4d4d1c03a76fd92acf33cf6322071b10de785
ItsJady-Taken <davidkhun2004@gmail.com>
2024-09-03 16:30:39 +0700

    Working on functionality of todo content

--- src/app/showTodoContent.js
+++ src/app/showTodoContent.js
@@ -1,19 +1,22 @@
-const content = document.getElementById('content');
+import projectModal, { removeProjectFirstChild } from "./projectModal";
+import { showProject } from "./createProject";
+import { loadTodoContent } from "./createTodo";
 
+const content = document.getElementById('content');
 function showTodoContent(title, description, date, priority) {
     const todoContent = document.createElement('div');
     todoContent.classList.add('todo-content');
+    todoContent.id = `todo-${title}-content`;
 
     todoContent.innerHTML = `
         <div class="todo-content-remove top-bottom">
-            <button class="back-btn"><i class="fa-solid fa-arrow-left-long"></i></button> <button class="remove-project-btn" >Remove Todo</button>
+            <button type="button" id="back-btn" class="back-btn"><i class="fa-solid fa-arrow-left-long"></i></button> 
+            <button class="remove-project-btn" id="remove-${title}">Remove Todo</button>
         </div>
         <div class="todo-content-title">      
-            
             <h1>${title}</h1>
         </div>
         <div class="todo-content-description">
-           
            <textarea cols="70" rows="30" name="description" id="description" disabled>${description}</textarea>
         </div>
         <div class="todo-content-date">
@@ -30,7 +33,34 @@ function showTodoContent(title, description, date, priority) {
        
     `;
     content.appendChild(todoContent);
+    const projectDropdownBtn = document.querySelector('#project-dropdown-btn');
+    const existingTodos = JSON.parse(localStorage.getItem(projectDropdownBtn.textContent)) || [];
+    const backBtn = document.querySelector('#back-btn');
+    backBtn.addEventListener('click', () => {  
+        removeProjectFirstChild();  
+        projectModal(existingTodos);
+        loadTodoContent(projectDropdownBtn.textContent);
+    });
+
+    const removeTodoBtn = document.getElementById(`remove-${title}`);
+     removeTodoBtn.addEventListener('click', () => {
+        removeTodoContent(title);
+        removeProjectFirstChild();
+        projectModal(existingTodos);
+        loadTodoContent(projectDropdownBtn.textContent);
+     })
     return todoContent;
+}
+
+function removeTodoContent(title) {
+    const projectDropdownBtn = document.querySelector('#project-dropdown-btn');
+    const existingTodos = JSON.parse(localStorage.getItem(projectDropdownBtn.textContent)) || [];
+    const targetTodo = existingTodos.projects.find(todo => todo.title === title);
+        const index = existingTodos.projects.indexOf(targetTodo);
+        console.log(targetTodo, index);
+        existingTodos.projects.splice(index, 1);
+        localStorage.setItem(projectDropdownBtn.textContent, JSON.stringify(existingTodos));
+        
 }
 
 export { showTodoContent };
